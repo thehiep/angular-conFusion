69f135bd1e9d31104f2b9df4fa3479c90047220b
Hiep Bui <hiep.bui@vn.woowahan.com>
2021-11-28 14:34:26 +0700

    basic service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,8 @@ import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { MatCardModule } from "@angular/material";
 import { MatButtonModule } from "@angular/material";
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { DishService } from "./services/dish.service";
 @NgModule({
   declarations: [AppComponent, MenuComponent, DishdetailComponent],
   imports: [
@@ -24,7 +24,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
