72fe88471c5f8c7b918c8bbd29b4ef7f875a1310
Hiep Bui <hiep.bui@vn.woowahan.com>
2021-11-28 16:14:14 +0700

    angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,32 @@ import { MenuComponent } from "./menu/menu.component";
 import { MatCardModule } from "@angular/material";
 import { MatButtonModule } from "@angular/material";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+
 import { DishService } from "./services/dish.service";
+
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { HomeComponent } from "./home/home.component";
+
 @NgModule({
-  declarations: [AppComponent, MenuComponent, DishdetailComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
+    AppRoutingModule,
     MatToolbarModule,
     MatListModule,
     MatGridListModule,
